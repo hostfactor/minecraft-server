b07728f19ad1ebfe52b0fd6a1a608723331b8911
edwardowens <eddiebeme@gmail.com>
2022-01-19 17:08:56 -0800

    Using the minecrafter library.

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,24 @@
-ARG OPENJDK_TAG=17-alpine
+ARG TAG=8-jre-slim
 
-FROM debian as builder
+FROM alpine/curl as builder
 
-ARG MINECRAFT_JAR_PATH="https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar"
+ARG ARTIFACT_URL="https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar"
 
 WORKDIR /app/tmp
 
-RUN apt-get update && apt-get install -y curl
+RUN curl ${ARTIFACT_URL} -o /app/tmp/server.jar
 
-RUN curl ${MINECRAFT_JAR_PATH} -o /app/tmp/server.jar
-
-FROM openjdk:${OPENJDK_TAG}
+FROM openjdk:${TAG}
 
 COPY --from=builder /app/tmp/server.jar /server/
 
 WORKDIR server
 
 RUN echo "eula=true" > eula.txt
+
+LABEL org.opencontainers.image.description='Minecraft Java Edition version ${VERSION}. See Changelog: ${VERSION_URL}.'
+LABEL org.opencontainers.image.url='ghcr.io/hostfactor/minecraft-server'
+LABEL org.opencontainers.image.version=${VERSION}
+LABEL org.opencontainers.image.authors='eddie@hostfactor.io'
 
 ENTRYPOINT ["java", "-jar", "server.jar"]
--- main.go
+++ main.go
@@ -1,109 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gocolly/colly/v2"
+	"github.com/hostfactor/minecrafter"
+	"github.com/hostfactor/minecrafter/edition"
 	"os"
-	"os/exec"
-	"path"
-	"strings"
 )
 
-type JDKTag struct {
-	Tag         string
-	DisplayName string
-	IsDefault   IsDefaultFunc
-	Skip        SkipFunc
-}
-
-type IsDefaultFunc func(version, tag string) bool
-type SkipFunc func(version, tag string) bool
-
-func Skip(version, tag string) bool {
-	return strings.HasPrefix(version, "1.18") && tag != "17-alpine"
-}
-
-func IsDefault(version, tag string) bool {
-	return strings.HasPrefix(version, "1.18") && tag == "17-alpine"
-}
-
-var OpenJDKTags = []JDKTag{
-	{Tag: "17-alpine", DisplayName: "java-17", Skip: Skip, IsDefault: IsDefault},
-	{Tag: "15-alpine", DisplayName: "java-15", Skip: Skip, IsDefault: IsDefault},
-	{Tag: "11-jre-slim", DisplayName: "java-11", Skip: Skip, IsDefault: IsDefault},
-	{Tag: "8-jre-slim", DisplayName: "java-8", Skip: Skip, IsDefault: IsDefault},
-}
-
 func main() {
-	c := colly.NewCollector(colly.UserAgent("Mozilla/5.0"))
+	edBuilder := minecrafter.New([]string{os.Getenv("GITHUB_REGISTRY")})
 
-	version := ""
-	link := ""
-	c.OnHTML("table.wikitable td a[href]", func(element *colly.HTMLElement) {
-		if !semverRegex.MatchString(element.Text) {
-			return
-		}
-
-		version = element.Text
-		err := element.Request.Visit(element.Attr("href"))
-		if err != nil {
-			panic(err.Error())
-		}
-	})
-
-	c.OnHTML(`a[href].external.text`, func(element *colly.HTMLElement) {
-		if element.Text != "Server" {
-			return
-		}
-		link = element.Attr("href")
-		if path.Ext(link) != ".jar" {
-			return
-		}
-
-		toPush := make([]string, 0, len(OpenJDKTags))
-		for _, v := range OpenJDKTags {
-			if v.Skip(version, v.Tag) {
-				fmt.Println("Skipping JDK", v.Tag, "for version", version, ": Incompatible.")
-				continue
-			}
-			fmt.Println("Building image for version", version, "jar path", link, "jdk tag", v)
-			tag := fmt.Sprintf("%s:%s-%s", os.Getenv("GITHUB_REGISTRY"), version, v.DisplayName)
-			if v.IsDefault(version, v.Tag) {
-				tag = fmt.Sprintf("%s:%s", os.Getenv("GITHUB_REGISTRY"), version)
-			}
-			cmd := exec.Command("docker", "build", "-t",
-				tag,
-				"--build-arg", fmt.Sprintf("MINECRAFT_JAR_PATH=%s", link),
-				"--build-arg", fmt.Sprintf("OPENJDK_TAG=%s", v.Tag),
-				".",
-			)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println("Failed to run", err.Error())
-			}
-			toPush = append(toPush, tag)
-		}
-
-		if len(toPush) == 0 {
-			fmt.Println("Nothing to push. Skipping.")
-			return
-		}
-
-		for _, v := range toPush {
-			fmt.Println("Pushing", v)
-			cmd := exec.Command("docker", "push", v)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println("Failed to push", err.Error())
-			}
-		}
-	})
-
-	err := c.Visit("https://minecraft.fandom.com/wiki/Java_Edition_version_history")
+	err := edBuilder.BuildEdition(new(edition.JavaEdition))
 	if err != nil {
 		panic(err.Error())
 	}
