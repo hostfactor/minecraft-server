30c80f3bedf93612ed2e7eb8e4c106996404a8e2
edwardowens <eddiebeme@gmail.com>
2022-01-23 11:44:49 -0800

    Adding version constraints to builds.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module minecraft-server
 go 1.16
 
 require (
-	github.com/hostfactor/minecrafter v0.0.0-20220120003843-8e384f9034ce
+	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/hostfactor/minecrafter v0.0.0-20220123193006-5c793c64218d
 	github.com/stretchr/testify v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/Masterminds/semver/v3"
 	"github.com/hostfactor/minecrafter"
 	"github.com/hostfactor/minecrafter/edition"
 	"os"
@@ -8,12 +11,23 @@ import (
 
 func main() {
 	edBuilder := minecrafter.New([]string{os.Getenv("GITHUB_REGISTRY")})
+	all := flag.Bool("all", false, "Build every Minecraft version")
+	flag.Parse()
+	args := flag.Args()
 
+	con, _ := semver.NewConstraint(">= 1.18")
+	opts := []minecrafter.BuildOpt{
+		minecrafter.WithSemverConstraint(con),
+	}
 	var err error
-	if len(os.Args) > 1 {
-		err = edBuilder.BuildRelease(new(edition.JavaEdition), os.Args[1])
+	if all != nil && *all {
+		fmt.Println("Building every Minecraft version.")
+		opts = []minecrafter.BuildOpt{}
+	}
+	if len(args) > 1 {
+		err = edBuilder.BuildRelease(new(edition.Java), os.Args[1], opts...)
 	} else {
-		err = edBuilder.BuildEdition(new(edition.JavaEdition))
+		err = edBuilder.BuildEdition(new(edition.Java), opts...)
 	}
 	if err != nil {
 		panic(err.Error())
